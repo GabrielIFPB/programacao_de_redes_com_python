f5aa13ca91be417887b8f3ba90ed2dc7fb015ed5
GabrielIFPB <gabriel.jeronimo@academico.ifpb.edu.br>
2020-09-22 23:38:18 -0300

    chave da api não estava sendo passada no construtor
    chave da api não estava sendo passada no construtor

--- capitulo01/search01.py
+++ capitulo01/search01.py
@@ -5,4 +5,4 @@ from pygeocoder import Geocoder
 if __name__ == '__main__':
 	address = '207 N. Defiance St, Archbold, OH'
 	key_api = ''
-	print(Geocoder().geocode(address)[0].coordinates)
+	print(Geocoder(key_api).geocode(address)[0].coordinates)
